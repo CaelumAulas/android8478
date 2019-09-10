ad0ed970cd65cc82b94dcbe0f999ba2c4e0f5779
Matheus Brandino <m.v.brandino@gmail.com>
2019-09-09 21:47:08 -0300

    aprendendo listener

--- app/src/main/java/br/com/caelum/twittelum/MainActivity.kt
+++ app/src/main/java/br/com/caelum/twittelum/MainActivity.kt
@@ -1,12 +1,27 @@
 package br.com.caelum.twittelum
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        salvar.setOnClickListener {
+
+            publicaTweet()
+
+        }
+    }
+
+    private fun publicaTweet() {
+        val texto = campoDeMensagem.text.toString()
+
+        Toast.makeText(this, texto, Toast.LENGTH_LONG).show()
     }
 }
